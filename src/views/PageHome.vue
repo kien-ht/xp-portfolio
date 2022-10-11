d2f9b284bbb8abe8a40f4445eb58d395dbca2f09
kien-ht <kienht.dev@gmail.com>
2022-10-12 00:41:14 +0700

    install xp.css

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -52,46 +52,5 @@
 <script setup lang="ts"></script>
 
 <style scoped>
-/* button {
-  border: none;
-  background: #ece9d8;
-  box-shadow: inset -1px -1px #0a0a0a, inset 1px 1px #fff, inset -2px -2px grey,
-    inset 2px 2px #dfdfdf;
-  border-radius: 0;
-  min-width: 75px;
-  min-height: 23px;
-  padding: 0 12px;
 
-  font-family: 'Pixelated MS Sans Serif', Arial;
-  -webkit-font-smoothing: antialiased;
-  font-size: 11px;
-  box-sizing: border-box;
-  border: 1px solid #003c74;
-  background: linear-gradient(180deg, #fff, #ecebe5 86%, #d8d0c4);
-  box-shadow: none;
-  border-radius: 3px;
-}
-
-button:not(:disabled):hover {
-  box-shadow: inset -1px 1px #fff0cf, inset 1px 2px #fdd889,
-    inset -2px 2px #fbc761, inset 2px -2px #e5a01a;
-}
-
-button.focused,
-button:focus {
-  box-shadow: inset -1px 1px #cee7ff, inset 1px 2px #98b8ea,
-    inset -2px 2px #bcd4f6, inset 1px -1px #89ade4, inset 2px -2px #89ade4;
-}
-
-button.focused,
-button:focus {
-  box-shadow: inset -1px 1px #cee7ff, inset 1px 2px #98b8ea,
-    inset -2px 2px #bcd4f6, inset 1px -1px #89ade4, inset 2px -2px #89ade4;
-}
-
-button.focused,
-button:focus {
-  outline: 1px dotted #000;
-  outline-offset: -4px;
-} */
 </style>
